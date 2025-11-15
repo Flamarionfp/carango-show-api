3173b7ecc9f7db0c25e7b54f91a6bdd21d099beb
Flamarionfp <flamarionpinto@sou.faccat.br>
2025-11-15 20:10:12 -0300

    chore: remove log

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,8 +10,6 @@ export interface DatabaseConnection {
 }
 
 export async function connectDatabase(): Promise<DatabaseConnection> {
-  console.log("DEBUG INSIDE", process.env.DATABASE_URL);
-
   const client = createClient({
     url: process.env.DATABASE_URL!,
     authToken: process.env.DATABASE_AUTH_TOKEN,
